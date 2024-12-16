215f1ce0967f9d306aaa5e97e53e5ec2a0ff36e4
kristian andersen <krilleandersen@gmail.com>
2024-12-16 13:16:56 +0100

    boing boimg

--- src/js/receiver.js
+++ src/js/receiver.js
@@ -1,5 +1,5 @@
 const receiver = () => {
-  // const NAMESPACE = 'urn:x-cast:com.screeninfo.app';
+  const NAMESPACE = 'urn:x-cast:com.screeninfo.app';
   const debug = document.getElementById('debug');
   const startTime = Date.now();
 
@@ -7,18 +7,41 @@ const receiver = () => {
     const time = ((Date.now() - startTime) / 1000).toFixed(1);
     const logMessage = `[${time}s] ${message}`;
 
-    debug.innerHTML = `${logMessage  }<br>${  debug.innerHTML}`;
+    debug.innerHTML = `${logMessage}<br>${debug.innerHTML}`;
+  };
+  log('receiver init');
+  // Initialize when the page loads
+  const update = data => {
+    //make url always an array
+    if (data.url) {
+      data.url = [].concat(data.url);
+      //this.setUrls(data.url);
+    }
+    //this.data = Object.assign(this.data, data);
+    //this.reflow();
   };
 
-  // Initialize when the page loads
-  window.addEventListener('load', () => {
-    if (cast && cast.framework) {
-      // initializeReceiver();
-      log('Succes: Cast API is available');
-    } else {
-      log('ERROR: Cast API not available');
-    }
-  });
+  const setupCast = () => {
+    log('setupCast');
+    const context = cast.framework.CastReceiverContext.getInstance();
+
+    context.addCustomMessageListener(NAMESPACE, event => {
+      update(event.data);
+
+      // send something back
+      context.sendCustomMessage(NAMESPACE, event.senderId, {
+        requestId: event.data.requestId,
+        data: 'data',
+        event,
+      });
+    });
+
+    const options = new cast.framework.CastReceiverOptions();
+    options.disableIdleTimeout = true; //no timeout
+    context.start(options);
+  };
+
+  setupCast();
 };
 
 export default receiver;
